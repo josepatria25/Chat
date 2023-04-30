1d79c6dac914953847ac75db2c792c4c3ba9f2fa
josepatria25 <josepatria92@gmail.com>
2023-04-30 19:53:26 -0400

    access control

--- src/components/Access.vue
+++ src/components/Access.vue
@@ -1,9 +1,64 @@
 <script setup>
+import useQuasar from "quasar/src/composables/use-quasar.js";
+import { ref } from "vue";
+
+/**
+ * access control
+*/
+const access = ref(true)
+
+/**
+ * User Informatio
+*/
+const objectForm = ref({
+  email: ref(""),
+  password: ref(""),
+  isPwd: ref(true),
+});
+
+/**
+ * Send data to firebase
+*/
+const sendData = () => {
+
+};
 
 </script>
 
 <template>
-    <div class="q-px-xl">
-        <div class="text-h5">Formulario de Registro/Login</div>
-    </div>
+  <div class="q-px-xl">
+    <div class="text-h5">Formulario de {{ access ? 'Login' : 'Registro'}} </div>
+    <q-form @submit="sendData" @reset="onReset" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="objectForm.email"
+        label="Your email *"
+        hint="Enter your email address"
+        lazy-rules
+        :rules="[(val) => !!val || 'Direccion de correo electronico requerido']"
+      />
+      <q-input
+        v-model="objectForm.password"
+        filled
+        :type="objectForm.isPwd ? 'password' : 'text'"
+        hint="Password with toggle"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="objectForm.isPwd = !objectForm.isPwd"
+          />
+        </template>
+      </q-input>
+
+      <div class="row justify-center q-gutter-md">
+        <q-btn style="width: 150px" label="Reset" type="reset" color="primary" />
+        <q-btn style="width: 150px" label="Login/Registro" type="submit" color="primary" />
+        <q-btn style="width: 150px" label="Ir a Registro" color="green" />
+        <q-btn style="width: 150px" label="Ir a Login" color="green" />
+
+      </div>
+    </q-form>
+  </div>
 </template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,11 @@
 <script setup>
+import Access from "components/Access.vue";
 
 </script>
 
 <template>
   <q-page padding>
-    
+    <Access/>
   </q-page>
 </template>
 
