f16c5cb9a769a6c26d21b6beca14d132bf2bca83
josepatria25 <josepatria92@gmail.com>
2023-05-01 18:35:36 -0400

    is autenticate

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,14 +2,16 @@
 import Access from "components/Access.vue";
 import ChatMessage from "components/ChatMessage.vue";
 import { auth } from "../boot/firebase.js";
+import { useAuth } from '@vueuse/firebase/useAuth'
+const { isAuthenticated, user } = useAuth(auth)
 
 
 </script>
 
 <template>
   <q-page padding>
-    <Access />
-    <ChatMessage/>
+    <Access v-if="!isAuthenticated"/>
+    <ChatMessage v-if="isAuthenticated"/>
   </q-page>
 </template>
 
