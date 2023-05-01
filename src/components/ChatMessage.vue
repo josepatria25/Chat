ac1e48cecdec819c7014d0120d7e5a4ffea80daa
josepatria25 <josepatria92@gmail.com>
2023-05-01 12:58:47 -0400

    qchatmessage

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-    <div>
-        
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        name="me"
+        :text="['hey, how are you?']"
+        sent
+      />
+      <q-chat-message
+        name="Jane"
+        :text="['doing fine, how r you?']"
+      />
     </div>
+  </div>
 </template>
