054fc476d1bc3e07663caf957dee544505f469bc
josepatria25 <josepatria92@gmail.com>
2023-05-01 09:38:50 -0400

    watch user auth

--- src/components/Access.vue
+++ src/components/Access.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref } from "vue";
 import {
-  getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,36 @@
+<script setup>
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../boot/firebase.js";
+
+/**
+ * Log Out
+*/
+const logout = () => {
+  signOut(auth)
+    .then(() => {
+      console.log('asdasd');
+    })
+    .catch((error) => {
+      console.log('qweqwe');
+    });
+};
+
+/**
+ * Actual User
+*/
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    console.log(uid);
+  }
+});
+</script>
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-space/>
-        <q-btn flat color="white" label="Log Out" />
+        <q-space />
+        <q-btn flat color="white" label="Log Out" @click="logout"/>
       </q-toolbar>
     </q-header>
 
@@ -13,10 +39,7 @@
     </q-page-container>
 
     <q-footer elevated class="bg-indigo-10 text-white">
-      <q-toolbar>
-
-      </q-toolbar>
+      <q-toolbar> </q-toolbar>
     </q-footer>
-
   </q-layout>
 </template>
